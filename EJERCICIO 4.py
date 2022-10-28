cfdd770b06895241ebae05a12cf4e40bcb9a6e0c
Jose <al_jperaltaa@iessierradelasvillas.org>
2022-10-28 10:58:06 +0200

    EJER 4 HECHO

--- EJERCICIO 4.py
+++ EJERCICIO 4.py
@@ -2,8 +2,22 @@
 Realizar un algoritmo que pida números (se pedirá por teclado la cantidad de
 números a introducir). El programa debe informar de cuantos números
 introducidos son mayores que 0, menores que 0 e iguales a 0.'''
-num=0
-num=int(input("Cuantos numeros deseas introducir:"))
+num_int=0
+num_int=int(input("Cuantos numeros deseas introducir:"))
 num1=0
-for num in num1:
-    print("Dime",num,"numeros:")
+contador_num_mayores=0
+contador_num_menores=0
+contador_num_iguales=0
+num=int
+while num1!=num_int:
+    num=int(input("Dime numeros:"))
+    if num>0:
+        contador_num_mayores+=1
+    if num==0:
+        contador_num_iguales+=1
+    if num<0:
+        contador_num_menores+=1
+    num1+=1
+print("Hay", contador_num_menores,"números menores que 0 \n")
+print("Hay", contador_num_iguales,"números iguales que 0 \n")
+print("Hay", contador_num_mayores,"números mayores que 0 \n")
