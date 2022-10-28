a8969f78c8776d539f99e18124ace20265ecc9aa
Jose <al_jperaltaa@iessierradelasvillas.org>
2022-10-28 16:34:11 +0200

    EJERCICIO 5 HECHO

--- EJERCICIO 5.py
+++ EJERCICIO 5.py
@@ -1,3 +1,10 @@
 '''Ejercicio 5
 Algoritmo que pida caracteres e imprima ‘VOCAL’ si son vocales y ‘NO VOCAL’
 en caso contrario, el programa termina cuando se introduce un espacio.'''
+caracter=str
+while caracter!=" ":
+    caracter=str(input("Dime un caracter en minuscula"))
+    if (caracter=="a")or (caracter=="e") or (caracter=="i") or (caracter=="o") or (caracter=="u"):
+        print("Vocal")
+    else:
+        print("No Vocal")
