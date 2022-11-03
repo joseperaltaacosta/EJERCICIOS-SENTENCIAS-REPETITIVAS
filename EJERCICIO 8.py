316d9824db23cb309457d64a2ce2031578b420ce
Jose <al_jperaltaa@iessierradelasvillas.org>
2022-11-03 10:15:32 +0100

    EJER 8 Y EMPEZAR 9

--- EJERCICIO 8.py
+++ EJERCICIO 8.py
@@ -6,9 +6,4 @@ Cuando termine el programa dará las siguientes informaciones:
 -La suma de los números que están dentro del intervalo (intervalo abierto).
 -Cuantos números están fuera del intervalo.
 -He informa si hemos introducido algún número igual a los límites del intervalo.'''
-lim_inf=0
-lim_sup=0
-lim_inf=int(input("Dime el limite inferior de un intervalo:"))
-lim_sup=int(input("Dime el limite superior de un intervalo:"))
-while (lim_inf>lim_sup) or (lim_inf!=0):
-    int(input("Dime el limite inferior de un intervalo:"))
+
---
+++ EJERCICIO 9.py
@@ -0,0 +1,8 @@
+'''Ejercicio 9
+Escribe un programa que dados dos números, uno real (base) y un entero
+positivo (exponente), saque por pantalla el resultado de la potencia. No se puede
+utilizar el operador de potencia.'''
+base=0
+exponente=0
+base=int(input("Dime la base de la potencia:"))
+exponente=int(input("Dime el exponente de la potencia:"))
