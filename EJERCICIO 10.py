42567319a08580aaf8de1ae6af54151f6840aeb6
Jose <al_jperaltaa@iessierradelasvillas.org>
2022-11-04 11:15:15 +0100

    EJER 9,10 Y EMPEZAR 11

---
+++ EJERCICIO 10.py
@@ -0,0 +1,6 @@
+'''Ejercicio 10
+Algoritmo que muestre la tabla de multiplicar de los números 1,2,3,4 y 5.'''
+for num in range(1,6):
+    print("****************")
+    for num1 in range(1,11):
+        print(num1,"*",num,"es:",num1*num)
--- EJERCICIO 9.py
+++ EJERCICIO 9.py
@@ -2,7 +2,9 @@
 Escribe un programa que dados dos números, uno real (base) y un entero
 positivo (exponente), saque por pantalla el resultado de la potencia. No se puede
 utilizar el operador de potencia.'''
-base=0
-exponente=0
 base=int(input("Dime la base de la potencia:"))
+potencia=base
 exponente=int(input("Dime el exponente de la potencia:"))
+for i in range(exponente-1):
+    potencia*=base
+print("El resultado de la potencia es:",potencia)
