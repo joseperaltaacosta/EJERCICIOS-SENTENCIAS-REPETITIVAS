4e689956cab45a70c5c0e992029589c314c86342
Jose <al_jperaltaa@iessierradelasvillas.org>
2022-11-15 14:34:30 +0100

    TERMINADA LA RELACION DE EJER

--- EJERCICIO 18.py
+++ EJERCICIO 18.py
@@ -1,6 +1,6 @@
 '''Ejercicio 18
 Hacer un programa que muestre un cronometro, indicando las horas, minutos y
-segundos.'''
+segundos.
 h=0
 min=0
 seg=0
@@ -16,4 +16,16 @@ for h in range(24):
         print("**********")
         for seg in range(60):
             seg+=1
-            print(seg,"segundos")
+            print(seg,"segundos")'''
+            
+import time
+horas=0
+minutos=0
+segundos=0
+for h in range(24):
+    for m in range(60):
+        for s in range(60):
+            print(f"{h}:{m}:{s}")
+            for i in range(7):
+                print()
+            time.sleep(1)
--- EJERCICIO 20.py
+++ EJERCICIO 20.py
@@ -2,16 +2,23 @@
 Mostrar en pantalla los N primero número primos. Se pide por teclado la cantidad
 de números primos que queremos mostrar.'''
 
-contador_=1
-n=0
-n=int(input("Dime la cantidad de numeros primos que quieres que te diga:"))
-num=input("")
-while contador_!=0:
-        contador=0
-for i in range(2,num):
-    if num%i==0:
-            contador=contador+1
-    if contador>=1:
-            print("El",num, "no es primo")
-    else:
-            print("El",num, "es primo")
+vNum=[]
+num_primo=0
+numero_primo=(int(input("Dime la cantidad de numeros primos que quieres que te muestre:")))
+i=2
+contador=numero_primo
+def num_primo(num):
+        num_primo=0
+        for i in range(2,num):
+                if num%i==0:
+                        num_primo+=1
+                if num_primo>=1:
+                        return False
+                else:
+                        return True
+while contador!=0:
+        if num_primo(i)==True:
+                vNum.append(i)
+                contador-=1
+        i+=1
+print(vNum)
